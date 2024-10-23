6df4e10b7cbf6114ca8b6631c8985babb8a3f16e
sw2park <pswoo990803@gmail.com>
2024-10-23 16:31:01 +0900

    24.10.23 초기 세팅

---
+++ LoaInfo/src/test/java/com/toy/LoaInfo/LoaInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.LoaInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
